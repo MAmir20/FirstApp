4d5699ced8b087733e4453240e528f8465b18cc0
MAmir20 <mezghani.amir@gmail.com>
2024-09-23 17:01:25 +0100

    configured app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MemberComponent } from './member/member.component';
+import { MemberFormComponent } from './member-form/member-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: MemberComponent
+  },
+  {
+    path: 'create',
+    pathMatch: 'full',
+    component: MemberFormComponent
+  },
+  {
+    path: '**',
+    component: MemberComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
